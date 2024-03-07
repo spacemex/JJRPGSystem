b1fd53f26006839ebf296c8f59d5d03f12be976f
Spacemex <devenshona@gmail.com>
2024-03-06 19:27:28 -0600

    Add item reference interfaces for inventory and pickup
    This commit introduces two new interfaces, GetPickupItemReference and GetInventoryItemReference, in the JJRPGSystem plugin. Each offers methods allowing other classes or objects to obtain a reference to either inventory or pickup items. These serve as a connection bridge between the game objects and items in the inventory or on the map.

---
+++ Source/JJRPGSystem/Private/Interfaces/GetInventoryItemReference.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "GetInventoryItemReference.generated.h"
+
+class ABaseInventoryItem;
+// This class does not need to be modified.
+UINTERFACE()
+class UGetInventoryItemReference : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class JJRPGSYSTEM_API IGetInventoryItemReference
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+
+	UFUNCTION(BlueprintCallable,BlueprintNativeEvent)
+	ABaseInventoryItem* GetInventoryItemReference();
+};
---
+++ Source/JJRPGSystem/Private/Interfaces/GetPickupItemReference.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GetPickupItemReference.h"
+#include "Actors/BasePickupItem.h"
+
+
+
+// Add default functionality here for any IGetPickupItemReference functions that are not pure virtual.
