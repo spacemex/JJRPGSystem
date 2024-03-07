074e81f6ecbfe0e1e418f96578037067ba93e780
Spacemex <devenshona@gmail.com>
2024-03-06 19:27:10 -0600

    Add InventorySystem component to RPG system
    This commit introduces an InventorySystem to the RPG system. This component includes functions to add items and update weight in the inventory. Also introduced are functions to get the current contents of the inventory and the current weight. This change allows inventory management in the RPG system.

---
+++ Source/JJRPGSystem/Public/Components/InventorySystem.h
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Data/FInventoryInfo.h"
+#include "Components/ActorComponent.h"
+#include "InventorySystem.generated.h"
+
+
+class ABasePickupItem;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class JJRPGSYSTEM_API UInventorySystem : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UInventorySystem();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditDefaultsOnly)
+	UDataTable* ItemsTable = nullptr;
+
+	//The Array That Holds The Contents Of Our Inventory.
+	UPROPERTY(EditDefaultsOnly)
+	TArray<FInventoryInfo> Contents;
+
+	UPROPERTY(EditDefaultsOnly)
+	float CurrentWeight = 0.0f;
+
+	UFUNCTION(BlueprintNativeEvent,BlueprintCallable)
+	bool AddItem(ABasePickupItem* ItemPickup, FName RowName);
+	
+	UFUNCTION(BlueprintPure)
+	TArray<FInventoryInfo> GetInventoryContents()
+	{
+		return Contents;
+	}
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateWeight(FName RowName);
+
+	UFUNCTION(BlueprintPure)
+	float GetCurrentWeight() const {return CurrentWeight;}
+};
