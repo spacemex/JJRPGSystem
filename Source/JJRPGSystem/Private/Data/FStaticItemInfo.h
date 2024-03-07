634e82664998c14e09c6ca0a0b6730fa15d3ced4
Spacemex <devenshona@gmail.com>
2024-03-06 19:27:36 -0600

    Add FStaticItemInfo structure
    A new structure, FStaticItemInfo has been added to the JJRPGSystem Plugin. This structure, which extends from FTableRowBase, contains properties for referencing a BasePickupItem and storing an item's weight. These properties are defined as UPROPERTY for compatibility with the Unreal Property System.

---
+++ Source/JJRPGSystem/Private/Data/FStaticItemInfo.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/BasePickupItem.h"
+#include "FStaticItemInfo.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(Blueprintable)
+struct FStaticItemInfo : public FTableRowBase
+{
+ GENERATED_BODY()
+
+ UPROPERTY(EditDefaultsOnly, Category = "Required")
+ TSubclassOf<ABasePickupItem> PickupRef;
+
+ UPROPERTY(EditDefaultsOnly, Category = "Weight")
+ float ItemWeight = 0.0f;
+
+
+};
